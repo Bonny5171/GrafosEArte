a5f28c9d9972506efe66927cbce8f7bb9e9d9b31
Luis Fernando <luis.city@gmail.com>
2018-07-20 01:51:19 -0300

    criando objetos

--- GrafosEArte/Program.cs
+++ GrafosEArte/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace GrafosEArte
 {
-    class Program
+    class Vertice
     {
+        public object[] ponto { get; set; }
+    }
+
+    /// <summary>
+    /// Aresta é a junção de duas faces
+    /// </summary>
+    class Aresta
+    {
+        public Dictionary<Vertice, Vertice> DuplaDeVetices { get; set; }
+    }
+
+    class Grafo
+    {
+        public Dictionary<Vertice, Aresta> ConjuntoDeVetice { get; set; }
+    }
+
+    class Program
+    {        
         static void Main(string[] args)
         {
+            //object grafo = (Vertice, Arestas);
         }
     }
 }
