334b26bba1d9330d2b28060759fa8682cdeb2536
Luis Fernando <luis.city@gmail.com>
2018-07-21 02:54:03 -0300

    alterado o tipo das arestas

--- GrafosEArte/Program.cs
+++ GrafosEArte/Program.cs
@@ -14,24 +14,22 @@ namespace GrafosEArte
     }
         
     class Aresta {
-        public Aresta(Vertice vertice1, Vertice vertice2)
-        {
-            Vertice[] aresta = new Vertice[2];
-            aresta[0] = vertice1;
-            aresta[1] = vertice2;
-            this.DuplaDeVetices = aresta;
+        public Aresta(Vertice pontoA, Vertice pontoB) {
+            this.pontoA = pontoA;
+            this.pontoB = pontoB;
         }
-        public Vertice[] DuplaDeVetices { get; set; }
+        public Vertice pontoA { get; set; }
+        public Vertice pontoB { get; set; }
     }
 
     class Grafo {
         public Grafo(Vertice[] vetices, Aresta[] aresta)
         {
             this.Vetices = vetices;
-            this.Aresta = aresta;
+            this.Arestas = aresta;
         }
         public Vertice[] Vetices { get; set; }
-        public Aresta[] Aresta { get; set; }
+        public Aresta[] Arestas { get; set; }
     }
 
     class Program
@@ -44,13 +42,14 @@ namespace GrafosEArte
             vertices[2] = new Vertice("Espirito Santo");
             vertices[3] = new Vertice("Rio de Janeiro");
 
-            Aresta aresta1 = new Aresta(vertices[0], vertices[1]);
-            Aresta aresta2 = new Aresta(vertices[0], vertices[3]);
-            Aresta aresta3 = new Aresta(vertices[1], vertices[2]);
-            Aresta aresta4 = new Aresta(vertices[1], vertices[3]);
-            Aresta aresta5 = new Aresta(vertices[2], vertices[3]);
+            Aresta[] arestas = new Aresta[5];
+            arestas[0] = new Aresta(vertices[0], vertices[1]);
+            arestas[1] = new Aresta(vertices[0], vertices[3]);
+            arestas[2] = new Aresta(vertices[1], vertices[2]);
+            arestas[3] = new Aresta(vertices[1], vertices[3]);
+            arestas[4] = new Aresta(vertices[2], vertices[3]);
 
-            Grafo RegiaoSudeste = new Grafo(vertices, Arestas);
+            Grafo RegiaoSudeste = new Grafo(vertices, arestas);
             Console.Write(RegiaoSudeste);
         }
     }
